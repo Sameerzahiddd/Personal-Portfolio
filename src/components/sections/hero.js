15542dbefe302aefc38d225bad6dfd0275ca228e
Sameer Zahid <sameerzahid@uni.minerva.edu>
2025-01-09 16:57:13 -0800

    bio update

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,11 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Sameer Zahid.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = (
+    <h3 className="big-heading">
+      I explore how we can use AI to supercharge education & make it more accessible/.
+    </h3>
+  );
   const four = (
     <>
       <p>
