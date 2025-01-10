cf85c76f74b10e9b139abd7615cfd9c6f94c109f
Sameer Zahid <sameerzahid@uni.minerva.edu>
2025-01-09 18:31:36 -0800

    updated picture

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -173,8 +173,8 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/g"
-              width={500}
+              src="../../images/me.jpeg"
+              width={400}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
               alt="Headshot"
